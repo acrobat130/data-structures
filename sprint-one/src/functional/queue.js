9875f65fc4dc3b6377e1e6e0f812406efcab7f6a
acrobat130 <shanna.sullivan@gmail.com>
2015-10-07 16:24:01 -0700

    solution to queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,34 @@
 var Queue = function(){
   var someInstance = {};
-
+  var counter = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.dequeue = function(){
+    if (counter > 0){
+      var temp = storage[0];
+      delete storage[0];
+      counter--;
+      
+        storage[0] = storage[1];
+      
+      return temp;
+    } 
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
 };
+
+
+
