d373ec22de74eb91e69cd0336cda4ad2aea8ae0c
acrobat130 <shanna.sullivan@gmail.com>
2015-10-10 11:14:06 -0700

    finished set!

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,32 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
+  set._counter = 0;
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[this._counter] = item;
+  this._counter++
 };
 
 setPrototype.contains = function(item){
+  var result = false;
+  for (var key in this._storage) {
+    if (this._storage[key] === item) {
+      result = true;
+      return result;
+    }
+  }
+  return result;
 };
 
 setPrototype.remove = function(item){
+  var temp = this._storage[this._counter - 1];
+  delete this._storage[this._counter - 1];
+  return temp;
 };
 
 /*
