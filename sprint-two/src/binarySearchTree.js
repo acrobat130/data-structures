f86d4daf0ea13f9fa9e3414fddb3f5821ce83ec7
acrobat130 <shanna.sullivan@gmail.com>
2015-10-10 11:42:42 -0700

    started binary tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,21 @@
 var BinarySearchTree = function(value){
+  var newBinaryTree = {};
+  newBinaryTree.left = null;
+  newBinaryTree.right = null;
+  newBinaryTree.children = [];
+  newBinaryTree.value = null;
+  _.extend(newBinaryTree, binaryTreeMethods);
+  return newBinaryTree; 
 };
+
+
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(value){
+  var temp = new BinarySearchTree(value);
+};
+binaryTreeMethods.contains = function(value){};
+binaryTreeMethods.depthFirstLog = function(callback){};
 
 
 /*
