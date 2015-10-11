64e2532915cc83422f374ce43af517a559cf839e
Shanna Sullivan <shanna.sullivan@gmail.com>
2015-10-10 18:33:44 -0700

    refactored most of hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,20 +5,37 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  //if (this._storage[i] === null) {
-    this._storage[i] = v;
-  //}
+    this._storage.set(i, v);
+    // if (!this._storage) {
+    //   this._storage[i]
+    // }
+    // this._storage[i] = [k,v];
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage[i];
+  //if (this._storage.length > 0 && this._storage.length < 2) {
+    return this._storage.get(i);
+
+  // } else if (this._storage.length > 1) {
+  //   console.log("do you see me");
+  //   for (var j = 0; j < this._storage.length; i++) {
+  //     if (k === this._storage[i][j]) {
+  //       return this._storage[i][j+1];
+  //     }
+  //   }
+  // }
 
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[i] = null;
+  this._storage.each(function(value, j, limitedArrayStorage) {
+    if (i === j) {
+      limitedArrayStorage[j] = null;
+    }
+  })
 };
 
 
